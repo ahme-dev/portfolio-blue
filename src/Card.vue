f8e65c04f8e9286f8d2e4a604e6dee5fae571cd0
Ahmed K. A <ahmadkabdullah@pm.me>
2022-09-27 14:05:38 +0300

    feat: switch to windicss

--- src/Card.vue
+++ src/Card.vue
@@ -3,7 +3,7 @@ const props = defineProps<{ title: string; points: string[] }>();
 </script>
 
 <template>
-  <div class="bg-orange-300 p-4 md:px-20 w-full md:w-1/2">
+  <div class="bg-orange-300 p-4 md:px-20 md:w-1/2 w-full">
     <h1 class="text-3xl bold">{{ props.title }}</h1>
     <p class="text-xl" v-for="point in props.points">
       {{ point }}
