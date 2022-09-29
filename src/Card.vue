7b3df315aa03e77459968c62b5f8e47da42d5c8f
Ahmed K. A <ahmadkabdullah@pm.me>
2022-09-29 23:09:31 +0300

    feat: add hover effect for each point

--- src/Card.vue
+++ src/Card.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="card" :class="{ 'card-dark': props.darkMode }">
+  <div class="card" @click="" :class="{ 'card-dark': props.darkMode }">
     <h1 class="title">{{ props.title }}</h1>
     <div class="points">
       <p class="point" v-for="point in props.points">
@@ -22,7 +22,7 @@ const props = defineProps<{
   @apply flex flex-col gap-4 
   @apply rounded-xl p-5 w-full
   @apply bg-true-gray-200 shadow-sm shadow-true-gray-500
-  @apply hover:( bg-gradient-to-b from-indigo-600 to-indigo-700 text-true-gray-100 cursor-pointer);
+  @apply hover:(bg-gradient-to-b from-indigo-600 to-indigo-700 text-true-gray-100 cursor-pointer);
 }
 .card-dark {
   @apply bg-true-gray-800 text-true-gray-100;
@@ -36,5 +36,6 @@ const props = defineProps<{
 .point {
   @apply rounded-lg p-2
   @apply bg-opacity-50 bg-true-gray-400;
+  @apply hover:(bg-indigo-200 text-true-gray-800);
 }
 </style>
